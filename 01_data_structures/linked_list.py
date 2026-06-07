660232dcedd5c5586548e074378585d70a5cd92f
marz <alzokarimarwa@gmail.com>
2026-06-07 18:25:21 -0500

    starting linked list tempalte added push back

--- 01_data_structures/linked_list.py
+++ 01_data_structures/linked_list.py
@@ -29,3 +29,24 @@ class LinkedList:
         self._size -= 1
         return popped_value
 
+    def push_back(self, value):
+        # Create the new node container
+        new_node = Node(value)
+
+        # Case 1: If the list is completely empty
+        if self.empty():
+            self.head = new_node
+
+        # Case 2: The list already has items
+        else:
+            current = self.head
+            # Loop until 'current' is sitting on the very last node
+            while current.next is not None:
+                current = current.next
+            # Link the last node's train hitch to our new node
+            current.next = new_node
+
+            # Don't forget to increase the size!
+        self._size += 1
+
+
