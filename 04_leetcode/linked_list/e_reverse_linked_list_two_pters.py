e0c58ca7b5499c144f43a67a417c69c45eae1012
marz <alzokarimarwa@gmail.com>
2026-06-15 11:09:17 -0500

    reverse linked list leetcode problem

---
+++ 04_leetcode/linked_list/e_reverse_linked_list_two_pters.py
@@ -0,0 +1,45 @@
+"""
+Problem:206. Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+
+URL: 
+Difficulty: [Easy]
+Pattern: two pointers
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        # while curr not null
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
