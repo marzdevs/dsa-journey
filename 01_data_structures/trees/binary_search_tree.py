755a8da83437c45683381b726ea575ffd9cc251d
marz <alzokarimarwa@gmail.com>
2026-06-23 16:23:04 -0500

    binary search tree from scratch

--- 01_data_structures/trees/binary_search_tree.py
+++ 01_data_structures/trees/binary_search_tree.py
@@ -3,3 +3,20 @@ class TreeNode:
         self.val = value
         self.left = None
         self.right = None
+
+    def insert(root, value):
+        # Your code goes here
+        if root is None:
+            root = TreeNode(value)
+            return root
+        else:
+            if value < root.val:
+                # Pass the value down to the left child,
+                # and capture whatever node it returns back into root.left
+                root.left = insert(root.left, value)
+            else:
+                # Do the exact same thing for the right side
+                root.right = insert(root.right, value)
+
+            # CRITICAL STEP: Always return the root at the end of the else block
+            return root
