784ae47c42b30aa8ac6a32f74ff5ef1178af8b6c
marz <alzokarimarwa@gmail.com>
2026-06-24 13:04:53 -0500

    binary search tree from scratch, added insert and insert helper method

--- 01_data_structures/trees/binary_search_tree.py
+++ 01_data_structures/trees/binary_search_tree.py
@@ -3,20 +3,42 @@ class TreeNode:
         self.val = value
         self.left = None
         self.right = None
+class BinarySearchTree:
+    def __init__(self):
+        # When a brand new tree is created, it starts completely empty
+        self.root = None
 
-    def insert(root, value):
+    def insert(self, value):
         # Your code goes here
-        if root is None:
-            root = TreeNode(value)
-            return root
+        if self.root is None:
+            self.root = TreeNode(value)
         else:
-            if value < root.val:
-                # Pass the value down to the left child,
-                # and capture whatever node it returns back into root.left
-                root.left = insert(root.left, value)
-            else:
-                # Do the exact same thing for the right side
-                root.right = insert(root.right, value)
+            self._insert_helper(self.root, value)
 
-            # CRITICAL STEP: Always return the root at the end of the else block
-            return root
+    def _insert_helper(self, current_node, value):
+        if value < current_node.val:
+            if current_node.left is None:
+                current_node.left = TreeNode(value)
+            else:
+                self._insert_helper(current_node.left, value)
+        else:
+            if current_node.right is None:
+                current_node.right = TreeNode(value)
+            else:
+                self._insert_helper(current_node.right, value)
+
+
+# Initialize your tree
+tree = BinarySearchTree()
+
+# Build the structure
+tree.insert(50)
+tree.insert(30)
+tree.insert(70)
+tree.insert(20)
+
+# Print values directly by following your pointer paths
+print("Root:", tree.root.val)          # Should be 50
+print("Left:", tree.root.left.val)     # Should be 30
+print("Right:", tree.root.right.val)   # Should be 70
+print("Far Left:", tree.root.left.left.val) # Should be 20
